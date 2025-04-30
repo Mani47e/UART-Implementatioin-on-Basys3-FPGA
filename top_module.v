10eff9b894e2e0ea76108c64e05a05fecb0a8095
Mani Chand A <146716967+Mani47e@users.noreply.github.com>
2025-04-30 19:01:08 +0530

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -3,28 +3,26 @@
 
 module top(
 input [7:0]sw,
+input [2:0]baudset,
 input btn0,
 input btn1,
 input btn2,
 input clk,
 input RxD,
 output TxD,
-//output TxD_debug,
-//output transmit_debug,
-//output button_debug, 
-//output clk_debug,
+
 output wire [7:0] LED
 ); 
 
 wire transmit;
-//assign TxD_debug = TxD;
-//assign transmit_debug = transmit;
-//assign button_debug = btn1;
-//assign clk_debug = clk;
 
 
 debounce D2 (.clk(clk), .btn(btn1), .transmit(transmit));
-transmitter T1 (.clk(clk), .reset(btn0),.transmit(transmit),.TxD(TxD),.data(sw));
-receiver R1(.clk(clk), .reset(btn2), .RxD(RxD), .RxData(LED));
+transmitter T1 (.clk(clk), .reset(btn0),.transmit(transmit),.TxD(TxD),.data(sw),.baudset(baudset));
+receiver R1(.clk(clk), .reset(btn2), .RxD(RxD), .RxData(LED),.baudrate_set(baudset));
+
+
+
+
 
 endmodule
